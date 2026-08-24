4662f79652aa62eae01cf9109baa7a08a4e5753a
Luan-dev-web <luan.fassinape@gmail.com>
2026-08-24 12:40:47 -0300

    ajustes-no-menu

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const menuToggle = document.getElementById("menu-toggle");
 const nav = document.querySelector("nav");
 const links = document.querySelectorAll(".links-menu");
+const header = document.querySelector("header");
+
 
 menuToggle.addEventListener("click", () => {
+
     nav.classList.toggle("ativo");
+    header.classList.toggle("menu-aberto");
+
 });
 
+
 links.forEach(link => {
+
     link.addEventListener("click", () => {
+
         nav.classList.remove("ativo");
+        header.classList.remove("menu-aberto");
+
     });
+
 });
